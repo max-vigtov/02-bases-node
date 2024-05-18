1a1a948fd7843250d179092964cc81ca94478d35
Max Vigueras <mvt.2000@hotmail.com>
2024-05-18 13:13:56 -0600

    Js: Callbacks lesson

--- src/js-foundation/02-destructuring.js
+++ src/js-foundation/02-destructuring.js
@@ -6,8 +6,8 @@ const { SHELL, HOMEBREW_PREFIX, npm_lifecycle_script } = process.env;
 
 // console.table({SHELL, HOMEBREW_PREFIX, npm_lifecycle_script});
 
-const characters = ['Flash', 'Superman', 'Batman'];
+const characters = ['Flash', 'Superman', 'Green Lantern', 'Batman'];
 
-const [_, __, batman] = characters;
+const [_, __,___, batman] = characters;
 
 // console.log(batman);
