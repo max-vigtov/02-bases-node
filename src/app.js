076d0ef93ca04f6bd97a5d80de020f91042afd35
Max Vigueras <mvt.2000@hotmail.com>
2024-05-20 15:52:24 -0600

    Functions: Arrow functions

--- src/app.js
+++ src/app.js
@@ -6,10 +6,9 @@ const { getUserById } = require('./js-foundation/03-callbacks')
 
 const id = 1;
 
-getUserById(id, function(error, user) {
- if (error) {
-   throw new Error( error )
-  }
+getUserById(id, (error, user) => {
+  
+ if (error) throw new Error( error )
 
   getUserById(2, function(error, user2) {
     if (error) {
