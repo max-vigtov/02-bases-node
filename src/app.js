6ddbd8294f76a3bc2ff23e96c7c3b2fba6bd3b9f
Max Vigueras <mvt.2000@hotmail.com>
2024-05-24 14:27:39 -0600

    Functions: Factory functions final draft

--- src/app.js
+++ src/app.js
@@ -1,19 +1,15 @@
 // const {emailTemplate} = require('./js-foundation/01-template');
 // require('./js-foundation/02-destructuring');
 //const { getUserById } = require('./js-foundation/03-callbacks');
-require ('./js-foundation/05-factory');
-// console.log(emailTemplate);
+//const getAge = require('get-age');
+const {getAge, getUUID} = require('./plugins');
+const {buildMakePerson} = require ('./js-foundation/05-factory');
 
-// const id = 1;
 
-// getUserById(id, (error, user) => {
+const makePerson = buildMakePerson({getUUID, getAge});
 
-//  if (error) throw new Error( error )
+const obj = { name: 'Max', birthdate: '2000-11-22' };
 
-//   getUserById(2, function(error, user2) {
-//     if (error) {
-//       throw new Error( error )
-//      }
-//   console.log({user, user2});
-//   });
-// });
+const john = makePerson(obj);
+
+console.log(john);
--- src/js-foundation/05-factory.js
+++ src/js-foundation/05-factory.js
@@ -1,17 +1,23 @@
-const {getAge, getUUID} = require('../plugins');
+//const {getAge, getUUID} = require('../plugins');
 
-const buildPerson = ({name, birthdate}) => {
+const buildMakePerson = ({getUUID, getAge}) => {
 
-  return {
-    id: getUUID(),
-    name: name,
-    birthdate: birthdate,
-    age: getAge(birthdate),
+  return({name, birthdate}) => {
+
+    return {
+      id: getUUID(),
+      name: name,
+      birthdate: birthdate,
+      age: getAge(birthdate),
+    }
   }
 }
+// const obj = { name: 'Max', birthdate: '2000-11-22' };
 
-const obj = { name: 'Max', birthdate: '2000-11-22' };
+// const john = buildPerson(obj)
 
-const john = buildPerson(obj)
+// console.log(john);
 
-console.log(john);
+module.exports = {
+  buildMakePerson
+} 
