a884c50d4ca226bcf9f47faa59fa95d71c5e0709
Max Vigueras <mvt.2000@hotmail.com>
2024-05-24 15:20:49 -0600

    Promises: Promises first draft

--- src/app.js
+++ src/app.js
@@ -1,15 +1,8 @@
-// const {emailTemplate} = require('./js-foundation/01-template');
-// require('./js-foundation/02-destructuring');
-//const { getUserById } = require('./js-foundation/03-callbacks');
-//const getAge = require('get-age');
 const {getAge, getUUID} = require('./plugins');
-const {buildMakePerson} = require ('./js-foundation/05-factory');
 
 
-const makePerson = buildMakePerson({getUUID, getAge});
+const getPokemonById = require('./js-foundation/06-promises');
 
-const obj = { name: 'Max', birthdate: '2000-11-22' };
-
-const john = makePerson(obj);
-
-console.log(john);
+ console.log(getPokemonById(4, (pokemon) => {
+    console.log(pokemon);
+ }));
