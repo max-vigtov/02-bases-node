1d8576fe328932a46e598b464092421783cab048
Max Vigueras <mvt.2000@hotmail.com>
2024-06-03 15:56:07 -0600

    Axios requests implemented

--- src/plugins/http-client.plugin.js
+++ src/plugins/http-client.plugin.js
@@ -1,10 +1,9 @@
-
-
+const axios = require('axios');
 
 const httpClientPlugin = {
   get: async(url) => {
-    const resp = await fetch(url);
-    return await resp.json();
+    const { data } = await axios.get(url);
+    return data;
   },
   
   post: async(url, body) => {},
