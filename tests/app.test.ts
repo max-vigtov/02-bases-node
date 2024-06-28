c0a325a5ebac197fae4221bd90e41cb590657012
Max Vigueras <mvt.2000@hotmail.com>
2024-06-28 17:11:56 -0600

    Test: Arrange, Act, Assert

---
+++ tests/app.test.ts
@@ -0,0 +1,15 @@
+
+
+describe('Test in the App File', () => {
+ it('should be true', () => {
+  // 1. Arrange
+
+  const num1 = 10
+  const num2 = 20
+
+  // 2. Act
+  const result = num1 + num2;
+  // 3. Assert
+  expect(result).toBe(30)
+  });
+});
